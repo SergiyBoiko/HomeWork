b8bef8f18752b5fc1fed1e1c4cb1e1276e9ac9ae
SergiyBoiko <codedva00@gmail.com>
2016-10-25 14:20:17 +0300

    create a regular expression to check the phone number

--- src/ValidateNumber.java
+++ src/ValidateNumber.java
@@ -1,7 +1,7 @@
-public class ValidateNumber {
 
+public class ValidateNumber {
 	public static boolean checkPhoneNumber(String telNumber) {
-		final String str = telNumber.replaceAll("[-+)(]","");
+		final String str = telNumber.replaceAll("[-+)(]","");//проверка на наличия знака
 		if(str.matches("^(\\d+)$")) {
 			if (str.length() < 10 ) {
 				System.out.println("Phone number short "+telNumber);
@@ -12,7 +12,12 @@ public class ValidateNumber {
 				return  false;
 			}
 		}
+		if(telNumber.matches("^(\\+|00)380(50|95|99|67|96|97|98|63|93|68|92|94|39)[\\d]{7}$") 
+				|| telNumber.matches("^0(50|95|99|67|96|97|98|63|93|68|92|94|39)[\\d]{7}$")){	
+		System.out.println("Phone number correct "+telNumber);
+			return true;
+		}
+		System.out.println("Phone number incorrect "+telNumber);
 		return false;
-
 	}
 }
