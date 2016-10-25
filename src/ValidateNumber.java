d9bf354eae25c5e6873490336dcbec67ed7259d8
SergiyBoiko <codedva00@gmail.com>
2016-10-25 14:17:28 +0300

    conditions validate the length of a phone number

--- src/ValidateNumber.java
+++ src/ValidateNumber.java
@@ -2,6 +2,16 @@ public class ValidateNumber {
 
 	public static boolean checkPhoneNumber(String telNumber) {
 		final String str = telNumber.replaceAll("[-+)(]","");
+		if(str.matches("^(\\d+)$")) {
+			if (str.length() < 10 ) {
+				System.out.println("Phone number short "+telNumber);
+				return false;
+			}
+			if(str.length() > 14){
+				System.out.println("Phone number long "+telNumber);
+				return  false;
+			}
+		}
 		return false;
 
 	}
