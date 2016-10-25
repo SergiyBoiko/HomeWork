932e1b494c7fa1d3b9e0ddb278418fb3fa93e63a
SergiyBoiko <codedva00@gmail.com>
2016-10-25 14:07:24 +0300

    design build

--- src/SimpleDesing.java
+++ src/SimpleDesing.java
@@ -1,3 +1,37 @@
-public class SimpleDesing {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
+public class SimpleDesing extends JFrame{
+	private static JButton button = new JButton("Check");
+	private JPanel panel = new JPanel();
+	private static JTextField inputField = new JTextField();
+	private static JTextArea outputField = new JTextArea();
+	private static JFrame window;
+	public SimpleDesing(){
+		super("ValidatePhone");
+		panel.setLayout(null);
+		button.setSize(80, 20);
+		button.setLocation(200,20);
+		panel.add(button);
+		inputField.setSize(170, 20);
+		inputField.setLocation(20,20);
+		panel.add(inputField);
+		outputField.setSize(260, 100);
+		outputField.setLocation(20,50);
+		outputField.setEditable(false);
+		panel.add(outputField);
+		setContentPane(panel);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);		
+		setSize(300, 200);
+
+	}
+	public static void main (String [] args) {
+		window = new SimpleDesing();
+		window.setVisible(true);
+	}
 }
